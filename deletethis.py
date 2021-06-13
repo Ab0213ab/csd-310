12501945a79ca4bd3c88e6ead94972e22ed3004c
Andrew Schaefer <81192828+Ab0213ab@users.noreply.github.com>
2021-06-13 09:01:15 -0400

    Delete deletethis.py

--- deletethis.py
+++
@@ -1,9 +0,0 @@
-#this is a test, delete this
-
-import pymongo 
-from pymongo import MongoClient 
-url = "mongodb+srv://admin:admin@cluster0.zcfrz.mongodb.net/pytech?retryWrites=true&w=majority"
-client = MongoClient(url)
-db = client.pytech
-print(db.list_collection_names())
-
